f928a83f45ccc2b4ec09acc604b0b06ad9c2e29b
sumeshdhonju <sumesh10.d@gmail.com>
2025-03-01 20:36:44 +0545

    Display

--- configure/urls.py
+++ configure/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('api/employee/', EmployeeListAPIView.as_view(), name='employee_list'),  # List & Create
     path('api/employee/<int:pk>/', EmployeeDetailAPIView.as_view(), name='employee_detail'),  # Retrieve, Update, Delete
     path('assign-shifts/', assign_shifts_api, name='assign_shifts_api'),
+    path("get-excel/", RetrieveExcelView.as_view(), name="get-excel"),
 ]
