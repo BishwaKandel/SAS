8bf6f1070ee893f562fd43372d5247d26d2995f8
sumeshdhonju <sumesh10.d@gmail.com>
2025-03-01 20:36:14 +0545

    Display

--- mail/views.py
+++ mail/views.py
@@ -4,6 +4,7 @@ from django.core.mail import send_mail
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+import os
 
 # def send_email_view(request): 
 #     send_mail_with_attachment(
@@ -42,11 +43,18 @@ from rest_framework import status
 class SendEmailView(APIView):
     def post(self, request):
         try:
+            file_path = "C:\\django\\SAS_final\\SAS\\Updated_Schdeule.csv",
+
+            if not os.path.exists(file_path):  
+                return Response({"error": "File not found!"}, status=status.HTTP_400_BAD_REQUEST)
+
+
             send_mail_with_attachment(
                 subject="Shift Allocated Table",
                 message="Please find the allocated shifts attached.",
-                file_path = "C:\\django\\SAS_final\\SAS\\Updated_Schdeule.csv",
+                file_path = file_path,
             )
             return Response({"message": "Email sent successfully!"}, status=status.HTTP_200_OK)
+        
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
