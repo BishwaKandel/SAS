deb708b4228a8fcaf10a6590a58017de5de7a77f
sumeshdhonju <sumesh10.d@gmail.com>
2025-03-01 14:03:57 +0545

    Employee

--- configure/urls.py
+++ configure/urls.py
@@ -10,7 +10,7 @@ from .views import *
 
 
 urlpatterns = [
-    path('api/employees/', EmployeesListAPIView.as_view(), name='employee_list'),  # List & Create
-    path('api/employees/<int:pk>/', EmployeesDetailAPIView.as_view(), name='employee_detail'),  # Retrieve, Update, Delete
+    path('api/employee/', EmployeeListAPIView.as_view(), name='employee_list'),  # List & Create
+    path('api/employee/<int:pk>/', EmployeeDetailAPIView.as_view(), name='employee_detail'),  # Retrieve, Update, Delete
     path('assign-shifts/', assign_shifts_api, name='assign_shifts_api'),
 ]
